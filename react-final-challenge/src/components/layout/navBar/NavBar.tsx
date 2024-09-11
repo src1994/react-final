c58fef4fd24d69fb9240848df9ee3b1387491d16
src1994 <sousamultimedia@gmail.com>
2024-09-11 13:39:30 +0100

    criação componente NavBar

--- react-final-challenge/src/components/layout/navBar/NavBar.tsx
+++ react-final-challenge/src/components/layout/navBar/NavBar.tsx
@@ -1,16 +1,21 @@
+import style from './NavBar.module.css';
+
 function NavBar() {
+
+    console.log(style)
+
     return (
         <header>
-        <div className="logo">NelsonMees</div>
-        <nav>
-            <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Works</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-        <button className="cta-button">Let's talk</button>
+            <div className={style['logo']}>Nelson<span className={style['logo-last-name']}>Mees</span></div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Works</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <button className={style['cta-button']}>Let's talk</button>
     </header>
     );
 }
